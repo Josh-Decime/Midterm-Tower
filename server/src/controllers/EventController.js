51f9df1804b87e34477d322acc43c8c156358ca1
Josh <josh_decime@yahoo.com>
2024-01-17 13:59:03 -0700

    post requests working, onto get

--- server/src/controllers/EventController.js
+++ server/src/controllers/EventController.js
@@ -1,4 +1,4 @@
-import auth0provider from "@bcwdev/auth0provider";
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { eventService } from "../services/EventService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -6,13 +6,17 @@ export class EventController extends BaseController {
     constructor() {
         super('api/events')
         this.router
+
+
+            .use(Auth0Provider.getAuthorizedUserInfo)
             .post('', this.createEvent)
     }
 
-    //TODO when I attempt to post on postman I'm getting back cant read properties of undefined reading id.. I'm not sure if that is a issue with me placing my bearer token in the wrong place in postman so I wont get a user, or if it is an issue with my code. I'll have to ask for help tomorrow since I cant continue until post works
+    \
     async createEvent(request, response, next) {
         try {
             const eventData = request.body
+            // FIXME 
             const userId = request.userInfo.id
             eventData.creatorId = userId
             const event = await eventService.createEvent(eventData)
